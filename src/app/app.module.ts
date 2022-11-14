1afd2d577a370069c4c7597cf28381cda46bd3f0
iago0 <posseiago@gmail.com>
2022-11-14 19:30:34 +0100

    pruebas topbar no funciona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SongListComponent } from './song-list/song-list.component';
 import { FormsModule } from '@angular/forms';
 import { SongDetailComponent } from './song-detail/song-detail.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppComponent,
     SongListComponent,
     SongDetailComponent,
-    NavBarComponent
+    NavBarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
